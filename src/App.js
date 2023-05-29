de1187484860f63c53e11d2e067d7b82a050f1d7
ShampaShaw <shampashaw088@gmail.com>
2023-05-29 16:05:55 +0530

    client file is created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Home from './container/Home'
+import Login from './components/login'
 
 const App = () => {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=21287ce169a6a9290f46e9e5de0ee13b`
   return (
     <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path="/*" element={<Home/>} />
     </Routes>
   )
