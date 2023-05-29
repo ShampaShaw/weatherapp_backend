701d818fea30eb5c58d0bcc95e8c451de4ad519d
ShampaShaw <shampashaw088@gmail.com>
2023-05-29 11:43:10 +0530

    login page created

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import videoBG from '../assets/earth_-_8947 (540p).mp4'
 
 const Home = () => {
   return (
     <>
       <div className='app'>
-      <div>
-        <button className='button'>search</button>
+      <div className='overlay'> </div>
+      <div className='video'>
+        <video src={videoBG} autoPlay loop muted />
+      <div className='contained'>
+      <div className='button'>
+      <form className='form'>
+        <input type='text' name='city' placeholder='Enter City...' id='myInput' />
+        <button id='myButton'>Submit</button>
+      </form>
       </div>
-        <div>
-          <h1>location</h1>
-          <img src='http://openweathermap.org/img/wn/' alt='weatherIcon'/>
-          <h3>cloudy</h3>
-          <h1 className='temperature'>32 degree celsius</h1>
+      <div>
+        <h1>location</h1>
+        <div className='icon' >
+        <img src='http://openweathermap.org/img/wn/02d@2x.png' alt='weatherIcon'/>
         </div>
+        <h3>cloudy</h3>
+        <h1 className='temperature'>32 degree celsius</h1>
+      </div>
+      </div>
+      </div>
       </div>
     </>
   )
