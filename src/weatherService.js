802b989d0585649bc29a4693a88b6fe46a13f423
ShampaShaw <shampashaw088@gmail.com>
2023-06-08 20:42:16 +0530

    api key generated

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,16 +1,14 @@
-const API_KEY= '21287ce169a6a9290f46e9e5de0ee13b'
+const API_KEY= '94d8a95d52697e9a3f45ce94d5bdb711'
 
 const makeIconURL = (iconId) =>`http://openweathermap.org/img/wn/${iconId}@2x.png`
 
-const getFormattedWeatherData = async ( city , units = 'metric') => {
-    const URL = `api.openweathermap.org/data/2.5/forecast/daily?id=${city}&appid={API_KEY}&units=${units}`
+const getFormattedWeatherData = async ( city , cnt=14, units = 'metric') => {
+    const URL = `api.openweathermap.org/data/2.5/forecast/daily?id=${city}&cnt=${cnt}&appid={API_KEY}&units=${units}`
     //const URL= `https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`
 
     const data = await fetch(URL)
     .then((res) => res.json())
     .then((data) => data);
-
-    console.log(data)
 
     const { 
         weather, 
@@ -43,7 +41,5 @@ const getFormattedWeatherData = async ( city , units = 'metric') => {
         name
     }
 }
-
-
 
 export { getFormattedWeatherData };
