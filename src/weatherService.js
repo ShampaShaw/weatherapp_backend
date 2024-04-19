f1fe4fbb979bae188e8c1bef6bc428809cf623a3
ShampaShaw <shampashaw088@gmail.com>
2024-04-19 20:16:10 +0530

    Update weatherService.js to use hardcoded API key

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,12 +1,9 @@
-
-
-const API_KEY = process.env.REACT_APP_API_KEY;
-const URL_WEB = process.env.REACT_APP_URL;
+const API_KEY = '21287ce169a6a9290f46e9e5de0ee13b';
 
 const makeIconURL = (iconId) => `http://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 const getFormattedWeatherData = async (city, cnt = 14, units = 'metric') => {
-  const URL = `${URL_WEB}weather?q=${city}&cnt=${cnt}&appid=${API_KEY}&units=${units}`;
+  const URL = `https:/api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
 
   const data = await fetch(URL)
     .then((res) => res.json())
